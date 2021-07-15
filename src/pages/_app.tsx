5a8b7ced13973040b69eece4046dd4995e005925
czernous <czernous_a@hotmail.com>
2021-07-15 13:53:49 +0300

    update theme management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,55 @@
 import type { AppProps } from 'next/app';
+import { createContext } from 'preact';
+import { useEffect, useReducer } from 'preact/hooks';
+import { themeReducer } from 'reducers/theme-reducer';
+import theme from 'styles/theme';
+
+const AppContext = createContext({});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const initialSate: any = {
+    data: {
+      style: theme.colors.dark,
+      name: 'DARK',
+    },
+  };
+
+  const [state, dispatch] = useReducer(themeReducer, initialSate);
+  const currentTheme = state;
+
+  const toggleTheme = () => {
+    let theme = window.localStorage.getItem('theme-type');
+    let color = theme === 'DARK' ? 'LIGHT' : 'DARK';
+    dispatch(`SET_${color}_THEME`);
+    window.localStorage.setItem('theme-type', color);
+    console.log(state, window.localStorage.getItem('theme-type'));
+  };
+
+  useEffect(() => {
+    console.log(currentTheme);
+    let theme = window.localStorage.getItem('theme-type');
+    if (!theme) {
+      console.log('asd');
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        dispatch('SET_DARK_THEME');
+        window.localStorage.setItem('theme-type', 'DARK');
+      } else {
+        dispatch('SET_LIGHT_THEME');
+        window.localStorage.setItem('theme-type', 'LIGHT');
+      }
+    } else {
+      dispatch(`SET_${theme}_THEME`);
+    }
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state, dispatch }}>
+      <Component
+        {...pageProps}
+        theme={currentTheme.data.style}
+        toggleTheme={toggleTheme}
+      />
+    </AppContext.Provider>
+  );
 }
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,15 @@
 import MainLayout from 'layouts/main-layout/MainLayout';
+import Navbar from 'components/Navbar';
 import Link from 'next/link';
-import theme from 'styles/theme';
-import { useReducer, useEffect } from 'preact/hooks';
-import { themeReducer } from 'reducers/theme-reducer';
 
-export default function Home() {
-  const initialSate: any = {
-    data: {
-      style: theme.colors.dark,
-      name: 'DARK',
-    },
-  };
-
-  const [state, dispatch] = useReducer(themeReducer, initialSate);
-
-  const toggleTheme = () => {
-    let theme = window.localStorage.getItem('theme-type');
-    let color = theme === 'DARK' ? 'LIGHT' : 'DARK';
-    dispatch(`SET_${color}_THEME`);
-    window.localStorage.setItem('theme-type', color);
-    console.log(state, window.localStorage.getItem('theme-type'));
-  };
-
-  useEffect(() => {
-    let theme = window.localStorage.getItem('theme-type');
-    if (!theme) {
-      console.log('asd');
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        dispatch('SET_DARK_THEME');
-        window.localStorage.setItem('theme-type', 'DARK');
-      } else {
-        dispatch('SET_LIGHT_THEME');
-        window.localStorage.setItem('theme-type', 'LIGHT');
-      }
-    } else {
-      dispatch(`SET_${theme}_THEME`);
-    }
-  }, []);
+export default function Home(props: any) {
+  const toggleTheme = props.toggleTheme;
 
   return (
-    <MainLayout theme={state.data.style}>
+    <MainLayout theme={props.theme}>
+      <Navbar toggleTheme={toggleTheme} />
       <div className="home">
-        Hello World. <button onClick={() => toggleTheme()}>change theme</button>
+        Hello World. <button onClick={toggleTheme}>change theme</button>
         <ul>
           <li>
             <Link href="/about">
