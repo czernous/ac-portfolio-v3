669be7133158fbe2a050c78876523cddf427557b
Anton Chernous <a.chernous@notamedia.ru>
2021-09-06 18:36:52 +0300

    add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,42 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@900&family=Open+Sans&display=swap"
           />
+          <meta name="robots" content="index, follow" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link
+            rel="android-chrome-icon"
+            sizes="512x512"
+            href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/android-chrome-512x512.png"
+          />
+          <link
+            rel="android-chrome-icon"
+            sizes="192x192"
+            href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/android-chrome-192x192.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/favicon-16x16.png"
+          />
+          <link
+            rel="manifest"
+            href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/site.webmanifest"
+          />
         </Head>
         <body>
           <Main />
