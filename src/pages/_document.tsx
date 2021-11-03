31664537073abc99bbb9d0aa0ce18dff11a7e00e
Anton Chernous <a.chernous@notamedia.ru>
2021-11-03 09:52:50 +0300

    add ga 1st attempt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -41,6 +42,20 @@ class MyDocument extends Document {
             rel="manifest"
             href="https://phounton.sirv.com/Images/ac-dev-portfolio/icons/site.webmanifest"
           />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-Y5JK4HW69N"
+          ></script>
+
+          <Script>
+            dangerouslySetInnerHTML=
+            {{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'G-Y5JK4HW69N');`,
+            }}
+          </Script>
         </Head>
         <body>
           <Main />
